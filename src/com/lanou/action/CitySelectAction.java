d0bcc0340eca41aeb054ba6fc3e15b7e533cf6f2
崔国程 <nanhaierbuku@qq.com>
2017-12-20 17:21:44 +0800

    崔国程添加了一个方法

--- src/com/lanou/action/CitySelectAction.java
+++ src/com/lanou/action/CitySelectAction.java
@@ -2,9 +2,7 @@ package com.lanou.action;
 
 import com.lanou.domain.Area;
 import com.lanou.domain.Server;
-import com.opensymphony.xwork2.ActionSupport;
-import net.sf.json.JSONArray;
-import org.apache.struts2.ServletActionContext;
+
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -14,11 +12,15 @@ import java.util.List;
 /**
  * Created by dllo on 17/10/27.
  */
-public class CitySelectAction extends ActionSupport {
-//    用来存储所有大区的集合
+public class CitySelectAction {
+    public static void main(String[] args) {
+        System.out.println("崔国程添加的方法");
+    }
+    
+    //    用来存储所有大区的集合
     private List<Area> areaList;
 
-//    定义变量:用来接收前段传过来的那个大区
+    //    定义变量:用来接收前段传过来的那个大区
     private int index;
     private List<Server> servers;
 
@@ -42,34 +44,6 @@ public class CitySelectAction extends ActionSupport {
             areaList.add(area);
         }
     }
-
-    public String playGame() {
-        return SUCCESS;
-    }
-
-    public String getServer() throws IOException {
-//        返回json数据
-//        获取选择服务器大区中服务器的数据
-        List<Server> servers = areaList.get(index).getServers();
-//        把集合数据转换成JSON数据,写入响应体中
-        String json = JSONArray.fromObject(servers).toString();
-        HttpServletResponse response = ServletActionContext.getResponse();
-        response.setContentType("application/json;charset=utf-8");
-        response.getWriter().write(json);
-        return null;
-    }
-
-
-//    使用struts2的方法向客户端返回json数据
-    public String getServerJson(){
-//        给需要转换的数据类兑现赋值,实际上就是获取大区中所有服务器的集合
-//        注意:数据类型必须是成员变量
-        servers = areaList.get(index).getServers();
-
-        return SUCCESS;
-    }
-
-
 
     public List<Area> getAreaList() {
         return areaList;
